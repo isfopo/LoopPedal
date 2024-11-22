c428f7030f81d72afb67397ddb97c3e15dff3711
Isaac Poole <isaacpoolemusic@gmail.com>
2024-11-22 08:56:10 -0500

    📝 docs(LoopTrack): add docstring to LoopTrack class constructor
    - provide detailed parameter descriptions for LoopTrack constructor
    - explain default value for long_press_duration
    
    ♻️ refactor(LoopTrack): enhance button press handling logic
    
    - add conditional structure for handling different modes in on_button_press method

--- script/LoopTrack.py
+++ script/LoopTrack.py
@@ -13,8 +13,23 @@ class LoopTrack:
     _pressed_at: Union[float, None]
 
     def __init__(
-        self, track: Track.Track, button: ButtonElement, long_press_duration: float
+        self,
+        track: Track.Track,
+        button: ButtonElement,
+        long_press_duration: float = 1000,
     ) -> None:
+        """
+        Initializes a new instance of the LoopTrack class.
+
+        Parameters:
+        track (Track.Track): The track associated with this loop track instance.
+        button (ButtonElement): The button element that will trigger actions on this loop track.
+        long_press_duration (float): The duration (in milliseconds) to consider a button press as a long press.
+                                      Defaults to 1000 milliseconds.
+
+        This constructor sets the initial mode of the track to "stop", assigns the provided track,
+        button, and long press duration. It also adds a listener to the button to handle button value changes.
+        """
         self.mode = "stop"
         self.track = track
         self.button = button
@@ -39,7 +54,14 @@ class LoopTrack:
                 self.on_button_press()
 
     def on_button_press(self) -> None:
-        pass
+        if self.mode == "stop":
+            pass
+        elif self.mode == "record":
+            pass
+        elif self.mode == "play":
+            pass
+        elif self.mode == "overdub":
+            pass
 
     def on_button_long_press(self) -> None:
         pass
