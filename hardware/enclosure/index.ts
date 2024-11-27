da1b7da69977c8a229892ab37aad0855a9910c90
Isaac Poole <isaacpoolemusic@gmail.com>
2024-11-27 09:36:42 -0500

    ✨ feat(enclosure): implement end section geometry calculation
    - add new SectionOptions interface for section customization
    - introduce sectionGeo function to generate section geometry
    - enhance geometry with line and arc primitives
    
    ♻️ refactor(enclosure): adjust rod diameter and section width calculation
    
    - update rod diameter conversion to 1/2 inch
    - modify section width calculation to consider end sections
    
    💄 style(enclosure): improve section extrusion thickness
    
    - increase dimensions thickness conversion from 1/32 to 1/16 inch

--- hardware/enclosure/index.ts
+++ hardware/enclosure/index.ts
@@ -1,19 +1,24 @@
-import { arc, circle } from "@jscad/modeling/src/primitives";
+import { arc, circle, line } from "@jscad/modeling/src/primitives";
 import { extrudeLinear } from "@jscad/modeling/src/operations/extrusions";
 import { expand } from "@jscad/modeling/src/operations/expansions";
+import { union } from "@jscad/modeling/src/operations/booleans";
 import convert from "convert";
+
+interface SectionOptions {
+  isEndSection: boolean;
+}
 
 const rod = {
   length: convert(2, "ft").to("mm"),
-  diameter: convert(3 / 4, "in").to("mm"),
+  diameter: convert(1 / 2, "in").to("mm"),
   nut: {
     height: convert(1 / 2, "in").to("mm"),
-    clearance: convert(1, "in").to("mm"),
   },
 };
 
 const section = {
-  width: rod.length / 4,
+  width: (isEndSection: boolean = false) =>
+    (rod.length - (isEndSection ? rod.nut.height : 0)) / 4,
 };
 
 const buttons = {
@@ -25,16 +30,38 @@ const dimensions = {
   face: convert(3, "in").to("mm"),
   back: convert(3 / 2, "in").to("mm"),
   angle: convert(80, "deg").to("rad"),
-  thickness: convert(1 / 32, "in").to("mm"),
+  thickness: convert(1 / 16, "in").to("mm"),
 };
 
-const body = () => {
+const sectionGeo = ({ isEndSection }: SectionOptions) => {
+  const shafts = [
+    arc({ radius: rod.diameter / 2 + dimensions.thickness / 2 }),
+    arc({
+      radius: rod.diameter / 2 + dimensions.thickness / 2,
+      center: [0, dimensions.face],
+    }),
+    arc({
+      radius: rod.diameter / 2 + dimensions.thickness / 2,
+      center: [-dimensions.back, 0],
+    }),
+  ];
+
+  const face = line([
+    [rod.diameter / 2 + dimensions.thickness / 2, dimensions.face],
+    [rod.diameter / 2 + dimensions.thickness / 2, 0],
+  ]);
+
+  const back = line([
+    [-dimensions.back, -(rod.diameter / 2 + dimensions.thickness / 2)],
+    [0, -(rod.diameter / 2 + dimensions.thickness / 2)],
+  ]);
+
   return extrudeLinear(
-    { height: section.width },
-    expand({ delta: dimensions.thickness }, arc({ radius: rod.diameter / 2 }))
+    { height: section.width(isEndSection) },
+    expand({ delta: dimensions.thickness }, ...shafts, face, back)
   );
 };
 
 export const main = () => {
-  return body();
+  return sectionGeo({ isEndSection: true });
 };
